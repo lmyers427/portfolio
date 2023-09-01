bcb1360adae40e4a11bbd9d9a6e2e8540890d431
Lauren Myers <lmmyers2@mail.fhsu.edu>
2023-09-01 09:55:35 -0600

    Committing changes to profile project

---
+++ src/components/About.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name='about' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+
+        <div className='flex flex-col justify-center items-center w-full h-full'>
+
+            <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
+                <div className='sm:text-right pb-8 pl-4'>
+                    <p className='text-4xl font-bold inline border-b-4 border-[#58b6d8]'>About</p>
+                </div>
+            </div>
+            <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
+           <div>
+            <p className='sm:text-right text-4xl font-bold'>
+                Hi, my name is Lauren, nice to meet you, Please take a look around.
+            </p>
+            </div>
+            <div>
+                <p>
+                    I am a recent graduate of Fort Hays State University with a Bachelors degree in Computer Science. 
+                    I am skilled in a variety of programming languages and have taken on various projects 
+                    individually and with colleugues to expand my knowledge of web design and ultimately learn how to provide clients with high quality work.  
+                    I am passionate about producing exceptional work and strive to improve businesses one line of code at a time.   
+                </p>
+            </div>
+            </div>
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/Skills.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import Javascript from '../assets/javascript.png'
+import ReactImg from '../assets/react.png'
+import Node from '../assets/node.png'
+import FireBase from '../assets/firebase.png'
+import Python from '../assets/python.png'
+import Tailwind from '../assets/tailwind.png'
+import SQL from '../assets/sql.png'
+import Mongo from '../assets/mongo.png'
+import HTML from '../assets/html.png'
+import CSS from '../assets/css.png'
+
+const Skills = () => {
+  return (
+    <div name='skills' class='w-full h-screen bg-[#0a192f] text-gray-300'>
+        {/**Container */}
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+            <div>
+                <p className='text-4xl font-bold inline border-b-4 border-[#58b6d8]'>Skills</p>
+                <p className='py-4'>// These are the technologies I have worked with</p>
+            </div>
+
+            <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+                <div className='shadow-md shaddow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={HTML} alt="HTML icon" />
+                <p className='my-4'>HTML</p>
+                </div>
+
+                <div className='shadow-md shaddow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={Javascript} alt="Javascript icon" />
+                <p className='my-4'>Javascript</p>
+                </div>
+
+                <div className='shadow-md shaddow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={CSS} alt="CSS icon" />
+                <p className='my-4'>CSS</p>
+                </div>
+
+                <div className='shadow-md shaddow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={Python} alt="Python icon" />
+                <p className='my-4'>Python</p>
+                </div>
+
+                <div className='shadow-md shaddow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={ReactImg} alt="ReactImg icon" />
+                <p className='my-4'>React</p>
+                </div>
+
+                <div className='shadow-md shaddow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={Node} alt="Node icon" />
+                <p className='my-4'>NodeJS</p>
+                </div>
+
+                <div className='shadow-md shaddow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={Tailwind} alt="Tailwind icon" />
+                <p className='my-4'>Tailwindcss</p>
+                </div>
+
+                <div className='shadow-md shaddow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={Mongo} alt="Mongo icon" />
+                <p className='my-4'>Mongo DB</p>
+                </div>
+                
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Skills
