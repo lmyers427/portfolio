8a0ee3d8a1db002070d5ca3384679fc8c8b77320
Lauren Myers <lmmyers2@mail.fhsu.edu>
2023-09-21 13:13:58 -0600

    netlify form change

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,12 +2,35 @@ import React from 'react'
 
 
 
+
+
+
 const Contact = () => {
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const form = e.target;
+    try {
+      const response = await fetch('/', {
+        method: 'POST',
+        body: new FormData(form),
+      });
+      if (response.ok) {
+        // Handle successful submission
+        console.log('Form submitted successfully');
+      } else {
+        // Handle submission errors
+        console.error('Form submission failed');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
 
 
   return (
     <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
-      <form name="contact" method='POST' action="https://getform.io/f/c7673a7d-6066-4880-b0b9-c9a103ac752d" className='flex flex-col max-w-[600px] w-full'>
+      <form name="contact" method='POST' data-netlify="true" onSubmit={handleSubmit}  className='flex flex-col max-w-[600px] w-full'>
 
         <div className='pb-8'>
             <p className='text-4xl font-bold inline border-b-4 border-[#58b6d8] text-gray-300'>Contact</p>
