4a7ea785f50c2be7a3ca679ea8876bdde56e3b5a
Lauren Myers <lmmyers2@mail.fhsu.edu>
2023-09-21 14:54:02 -0600

    Issue with form submission netlify continue

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -30,8 +30,8 @@ const Contact = () => {
   };
 
   return (
-    <div name="contact" className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
-      <form name="contact" method='post' data-netlify="true"  onSubmit={handleSubmit}  className='flex flex-col max-w-[600px] w-full'>
+    <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
+      <form name='contact' method='post' data-netlify="true"  onSubmit={handleSubmit}  className='flex flex-col max-w-[600px] w-full'>
 
         <div className='pb-8'>
             <p className='text-4xl font-bold inline border-b-4 border-[#58b6d8] text-gray-300'>Contact</p>
