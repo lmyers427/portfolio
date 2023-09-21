01507f5d493ccbf7bf49758e4a40821cd740c302
Lauren Myers <lmmyers2@mail.fhsu.edu>
2023-09-21 14:24:59 -0600

    Added hidden form to html file

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,37 +7,19 @@ import React from 'react'
 
 const Contact = () => {
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const form = e.target;
-    try {
-      const response = await fetch('/', {
-        method: 'POST',
-        body: new FormData(form),
-      });
-      if (response.ok) {
-        // Handle successful submission
-        console.log('Form submitted successfully');
-      } else {
-        // Handle submission errors
-        console.error('Form submission failed');
-      }
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  };
-
-
 
   return (
     <div className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
-      <form name="contact" method='POST' data-netlify="true" onSubmit={handleSubmit}  className='flex flex-col max-w-[600px] w-full'>
+      <form name="contact" method='post' data-netlify="true" data-netlify-honeypot="bot-field" onSubmit="submit"  className='flex flex-col max-w-[600px] w-full'>
 
         <div className='pb-8'>
             <p className='text-4xl font-bold inline border-b-4 border-[#58b6d8] text-gray-300'>Contact</p>
             <p className='text-gray-300 py-4'> Submit the form below or send me an email - lauren.m.scott427@gmail.com</p>
         </div>
         <input type="hidden" name="form-name" value="contact" />
+        <div hidden>
+          <input name="bot-field" />
+        </div>
          <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />   
          <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />   
         <textarea className='bg-[#ccd6f6] p-2' name="message" placeholder='Message' rows="10"></textarea>
