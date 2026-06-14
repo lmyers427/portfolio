81277a7883a54a21fb0d8ea2ac2c0988791ca2c8
Lauren Myers <lmmyers2@mail.fhsu.edu>
2026-06-13 22:26:43 -0600

    Adjusted sizing of pngs

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -34,7 +34,7 @@ const Work = () => {
                   <div className='pt-8 text-center'>
                     
                    
-                    <a href={USDAPDFPWR}>
+                    <a href="https://public.tableau.com/app/profile/lauren.myers5575/viz/PesticideAnalysis/USDAPesticideResidueAnalysis">
                       <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>View</button>
                     
                     </a>
